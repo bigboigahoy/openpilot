b347c547728edf14b5495c73fb6671d7af41dbf9
bigboigahoy <49489685+bigboigahoy@users.noreply.github.com>
2025-04-08 09:09:36 -0500

    Try two for silence

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -13,7 +13,7 @@ from openpilot.common.retry import retry
 from openpilot.common.swaglog import cloudlog
 
 from openpilot.system import micd
-from openpilot.selfdrive.ui.frogpilot.quiet_mode import QuietMode
+from openpilot.selfdrive.ui.quiet.quiet_mode import QuietMode
 
 from openpilot.selfdrive.frogpilot.frogpilot_variables import ACTIVE_THEME_PATH, ERROR_LOGS_PATH, RANDOM_EVENTS_PATH, get_frogpilot_toggles, params_memory
 
@@ -143,7 +143,7 @@ class Soundd(QuietMode):
     ret = np.zeros(frames, dtype=np.float32)
 
     # if self.current_alert != AudibleAlert.none:
-    if self.should_play_sound(self.current_alert):
+    if self.should_play_sound(self.current_alert) is True:
       num_loops = sound_list[self.current_alert][1]
       sound_data = self.loaded_sounds[self.current_alert]
       written_frames = 0
@@ -215,8 +215,6 @@ class Soundd(QuietMode):
       cloudlog.info(f"soundd stream started: {stream.samplerate=} {stream.channels=} {stream.dtype=} {stream.device=}, {stream.blocksize=}")
       while True:
         sm.update(0)
-
-        self.load_param()
 
         if sm.updated['microphone'] and self.current_alert == AudibleAlert.none: # only update volume filter when not playing alert
           if self.frogpilot_toggles.alert_volume_control:
