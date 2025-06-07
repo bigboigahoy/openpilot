5f0c57c7beba38d548083fdc6f5e9708c0378546
bigboigahoy <49489685+bigboigahoy@users.noreply.github.com>
2025-06-06 19:05:01 -0500

    Try to change github url for themes

--- frogpilot/assets/download_functions.py
+++ frogpilot/assets/download_functions.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 from openpilot.frogpilot.common.frogpilot_utilities import delete_file, is_url_pingable
 
-GITHUB_URL = "https://raw.githubusercontent.com/FrogAi/FrogPilot-Resources"
+GITHUB_URL = "https://raw.githubusercontent.com/bigboigahoy/FrogPilot-Resources"
 GITLAB_URL = "https://gitlab.com/FrogAi/FrogPilot-Resources/-/raw"
 
 def check_github_rate_limit():
