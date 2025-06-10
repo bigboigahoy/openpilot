85f532fa19bd808a0c8f648d506c72c85676f648
bigboigahoy <49489685+bigboigahoy@users.noreply.github.com>
2025-06-09 21:51:57 -0500

    Try bg color change to see if that was causing it

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -127,8 +127,8 @@ void AnnotatedCameraWidget::drawHud(QPainter &p, const cereal::FrogPilotPlan::Re
 
   // Header gradient
   QLinearGradient bg(0, UI_HEADER_HEIGHT - (UI_HEADER_HEIGHT / 2.5), 0, UI_HEADER_HEIGHT);
-  bg.setColorAt(0, QColor::fromRgbF(0, 0, 0, 0.45));
-  bg.setColorAt(1, QColor::fromRgbF(0, 0, 0, 0));
+  bg.setColorAt(0, QColor::fromRgbF(17, 17, 27, 0.45));
+  bg.setColorAt(1, QColor::fromRgbF(17, 17, 27, 0));
   p.fillRect(0, 0, width(), UI_HEADER_HEIGHT, bg);
 
   QString speedLimitStr = (speedLimit > 1) ? QString::number(std::nearbyint(speedLimit)) : "–";
