dfcaf3528f5a5d8774b864d2e9f3d6ac9daef9fa
bigboigahoy <49489685+bigboigahoy@users.noreply.github.com>
2025-06-22 13:55:31 -0500

    Add DTRv3 files

--- frogpilot/ui/qt/offroad/model_settings.cc
+++ frogpilot/ui/qt/offroad/model_settings.cc
@@ -200,7 +200,7 @@ FrogPilotModelPanel::FrogPilotModelPanel(FrogPilotSettingsWindow *parent) : Frog
             selectableModels.append(modelName);
           }
         }
-        selectableModels.append(modelFileToNameMap.value("kerrygold"));
+        selectableModels.append(modelFileToNameMap.value("down-to-ride-v3"));
         selectableModels.sort();
         selectableModels.prepend(modelFileToNameMap.value(QString::fromStdString(params_default.get("Model"))));
 
@@ -272,8 +272,8 @@ void FrogPilotModelPanel::showEvent(QShowEvent *event) {
     modelFileToNameMapProcessed.insert(availableModels[i], processModelName(availableModelNames[i]));
   }
 
-  modelFileToNameMap.insert("kerrygold", "Kerrygold 👀📡");
-  modelFileToNameMapProcessed.insert("kerrygold", "Kerrygold");
+  modelFileToNameMap.insert("down-to-ride-v3", "Down to Ride v3 👀📡");
+  modelFileToNameMapProcessed.insert("down-to-ride-v3", "Down to Ride v3");
 
   downloadableModels.clear();
   downloadableModels = availableModelNames;
--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -38,15 +38,17 @@ def plannerd_thread():
   frogpilot_toggles = get_frogpilot_toggles()
 
   classic_model = frogpilot_toggles.classic_model
+  radarless_model = frogpilot_toggles.radarless_model
+  tomb_raider = frogpilot_toggles.model == "down-to-ride-v3"
 
   while True:
     sm.update()
     if sm.updated['modelV2']:
-      longitudinal_planner.update(sm, frogpilot_toggles)
-      longitudinal_planner.publish(classic_model, sm, pm, frogpilot_toggles)
+      longitudinal_planner.update(radarless_model, tomb_raider, sm, frogpilot_toggles)
+      longitudinal_planner.publish(classic_model, tomb_raider, sm, pm, frogpilot_toggles)
       publish_ui_plan(sm, pm, longitudinal_planner)
 
-    # Update FrogPilot variables
+    # Update FrogPilot parameters
     if sm['frogpilotPlan'].togglesUpdated:
       frogpilot_toggles = get_frogpilot_toggles()
 
