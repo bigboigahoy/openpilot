d38050f47196b6a245d3e81ffd61929629aafab9
bigboigahoy <49489685+bigboigahoy@users.noreply.github.com>
2025-07-06 17:47:42 -0500

    Attempt to increase torque when below 30mph

--- selfdrive/car/mazda/values.py
+++ selfdrive/car/mazda/values.py
@@ -13,7 +13,6 @@ Ecu = car.CarParams.Ecu
 # Steer torque limits
 
 class CarControllerParams:
-  STEER_MAX = 800                # theoretical max_steer 2047
   STEER_DELTA_UP = 10             # torque increase per refresh
   STEER_DELTA_DOWN = 25           # torque decrease per refresh
   STEER_DRIVER_ALLOWANCE = 15     # allowed driver torque before start limiting
@@ -22,8 +21,9 @@ class CarControllerParams:
   STEER_ERROR_MAX = 350           # max delta between torque cmd and torque motor
   STEER_STEP = 1  # 100 Hz
 
-  def __init__(self, CP):
-    pass
+  def __init__(self, CP, vEgoRaw=100., frogpilot_toggles=None):
+    # Increase steer_max when below 30mph
+    self.STEER_MAX = 1500 if vEgoRaw < 11.41 else 800 # theoretical max_steer 2047
 
 
 @dataclass
