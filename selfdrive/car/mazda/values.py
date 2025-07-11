13637cc61099fda67b15e624d5e6d53f06749ffa
bigboigahoy <49489685+bigboigahoy@users.noreply.github.com>
2025-07-10 19:24:33 -0500

    Return steer limit to default

--- selfdrive/car/mazda/values.py
+++ selfdrive/car/mazda/values.py
@@ -13,6 +13,7 @@ Ecu = car.CarParams.Ecu
 # Steer torque limits
 
 class CarControllerParams:
+  STEER_MAX = 800
   STEER_DELTA_UP = 10             # torque increase per refresh
   STEER_DELTA_DOWN = 25           # torque decrease per refresh
   STEER_DRIVER_ALLOWANCE = 15     # allowed driver torque before start limiting
@@ -22,8 +23,9 @@ class CarControllerParams:
   STEER_STEP = 1  # 100 Hz
 
   def __init__(self, CP, vEgoRaw=100., frogpilot_toggles=None):
-    # Increase steer_max when below 30mph
-    self.STEER_MAX = 1500 if vEgoRaw < 11.41 else 800 # theoretical max_steer 2047
+    # # Increase steer_max when below 30mph
+    # self.STEER_MAX = 1500 if vEgoRaw < 11.41 else 800 # theoretical max_steer 2047
+    pass
 
 
 @dataclass
